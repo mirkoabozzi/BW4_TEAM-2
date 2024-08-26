9ec0cf271b15ddeacbf55d1695f4df5a3f0bb2ad
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-08-26 17:22:17 +0200

    Revert "modifiche app"
    This reverts commit e70a7bac7f8796d6e9cfc47247a9ed623e6a4b22.

--- src/main/java/team_2/Application.java
+++ src/main/java/team_2/Application.java
@@ -1,11 +1,8 @@
 package team_2;
 
 import com.github.javafaker.Faker;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import team_2.dao.*;
 import team_2.entities.*;
 import team_2.enums.StatoAbbonamento;
 import team_2.enums.StatoDistributori;
@@ -13,126 +10,208 @@ import team_2.enums.Tipo;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4_team_2");
-    private static final Faker fk = new Faker();
-    private static final Random random = new Random();
-
-    public static Supplier<RivenditoriAutorizzati> rivenditoriAutorizzatiCreateOne = () ->
-            new RivenditoriAutorizzati(fk.company().name());
-
+    static Faker fk = new Faker();
+    //createOne
+    public static Supplier<RivenditoriAutorizzati> rivenditoriAutorizzatiCreateOne = () -> new RivenditoriAutorizzati(fk.company().name());
+    static Random r = new Random();
     public static Supplier<DistributoriAutomatici> distributoriAutomaticiCreateOne = () -> {
         StatoDistributori[] statoDistributoriList = StatoDistributori.values();
-        return new DistributoriAutomatici(statoDistributoriList[random.nextInt(statoDistributoriList.length)]);
+        return new DistributoriAutomatici(statoDistributoriList[r.nextInt(statoDistributoriList.length)]);
     };
-
     public static Supplier<Utente> utenteCreateOne = () -> {
         LocalDate date = fk.date().birthday(12, 90).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        return new Utente(fk.name().firstName(), fk.name().lastName(), date, random.nextInt(111111111, 999999999));
+        return new Utente(fk.name().firstName(), fk.name().lastName(), date, r.nextInt(111111111, 999999999));
     };
-
     public static Supplier<Tessera> tesseraCreateOne = () -> {
-        LocalDate initialDate = LocalDate.now();
-        LocalDate endDate = initialDate.plusMonths(random.nextInt(1, 24));
-        return new Tessera(initialDate, endDate, random.nextBoolean());
+        LocalDate initialDate = fk.date().birthday(2, 5).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate endDate = fk.date().birthday(0, 2).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return new Tessera(initialDate, endDate, r.nextInt(0, 1) == 0);
     };
-
     public static Supplier<Abbonamento> abbonamentoCreateOne = () -> {
-        LocalDate date = LocalDate.now();
+        LocalDate date = fk.date().birthday(0, 10).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         StatoAbbonamento[] statoAbbonamentoList = StatoAbbonamento.values();
         Tipo[] tipoList = Tipo.values();
-        return new Abbonamento(date, statoAbbonamentoList[random.nextInt(statoAbbonamentoList.length)],
-                tipoList[random.nextInt(tipoList.length)]);
+        return new Abbonamento(date, statoAbbonamentoList[r.nextInt(statoAbbonamentoList.length)], tipoList[r.nextInt(tipoList.length)]);
     };
-
     public static Supplier<Biglietto> bigliettoCreateOne = () -> {
-        LocalDate date = LocalDate.now();
-        return new Biglietto(random.nextBoolean(), date);
+        LocalDate date = fk.date().birthday(0, 1).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return new Biglietto(r.nextInt(0, 1) == 0, date);
     };
 
-    private static <T> void createAndPersistEntities(Supplier<T> entitySupplier, String entityName, Scanner scanner, EntityManager em, Runnable daoAction) {
-        try {
-            int numOfEntities;
-            while (true) {
-                System.out.printf("Quanti %s vuoi creare?%n", entityName);
-                try {
-                    numOfEntities = Integer.parseInt(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Inserisci un numero valido.");
-                }
-            }
-
-            EntityTransaction transaction = em.getTransaction();
-            transaction.begin();
+    //create Object
+    public static List<PuntoDiEmissione> createPuntoDiEmissione() {
+        List<PuntoDiEmissione> puntoDiEmissioneList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int numOfPuntoDiEmissione;
+        while (true) {
+            System.out.println("Quanti punti di emissione vuoi creare?");
             try {
-                for (int i = 0; i < numOfEntities; i++) {
-                    T entity = entitySupplier.get();
-                    daoAction.run(); // Esegui l'azione del DAO qui
-                }
-                transaction.commit();
-                System.out.printf("%s creati e salvati nel database con successo.%n", entityName);
+                numOfPuntoDiEmissione = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Inserisci un numero valido");
+
             } catch (Exception e) {
-                if (transaction.isActive()) {
-                    transaction.rollback();
-                }
-                System.out.printf("Errore durante il salvataggio: %s%n", e.getMessage());
+                System.out.println("Errore: " + e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.printf("Errore durante la creazione di %s: %s%n", entityName, e.getMessage());
+
         }
+
+        System.out.println("1. Crea distributori automatici \n 2. Crea rivenditori autorizzati");
+        int choice;
+        try {
+            choice = Integer.parseInt(sc.nextLine());
+            switch (choice) {
+                case 1:
+                    for (int i = 0; i < numOfPuntoDiEmissione; i++) {
+                        puntoDiEmissioneList.add(distributoriAutomaticiCreateOne.get());
+                    }
+                    System.out.println("Distributori automatici creati con successo");
+                    break;
+                case 2:
+                    for (int i = 0; i < numOfPuntoDiEmissione; i++) {
+                        puntoDiEmissioneList.add(rivenditoriAutorizzatiCreateOne.get());
+                    }
+                    System.out.println("Rivenditori autorizzati creati con successo");
+                    break;
+                default:
+                    System.out.println("Scelta non valida riprova!");
+                    break;
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Inserisci un numero valido");
+
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+        sc.close();
+        return puntoDiEmissioneList;
     }
 
-    public static void createDistributoriAutomatici(Scanner scanner, PuntoDiEmissioneDAO dao, EntityManager em) {
-        createAndPersistEntities(distributoriAutomaticiCreateOne, "distributori automatici", scanner, em, () -> dao.save(distributoriAutomaticiCreateOne.get()));
+    public static List<Utente> createUtente() {
+        List<Utente> utenteList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int numOfUtente;
+        while (true) {
+            System.out.println("Quante persone vuoi creare?");
+            try {
+                numOfUtente = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Inserisci un numero valido");
+
+            } catch (Exception e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+        try {
+            for (int i = 0; i < numOfUtente; i++) {
+                utenteList.add(utenteCreateOne.get());
+            }
+            System.out.println("Persone create con successo");
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+
+        return utenteList;
     }
 
-    public static void createRivenditoriAutorizzati(Scanner scanner, PuntoDiEmissioneDAO dao, EntityManager em) {
-        createAndPersistEntities(rivenditoriAutorizzatiCreateOne, "rivenditori autorizzati", scanner, em, () -> dao.save(rivenditoriAutorizzatiCreateOne.get()));
+    public static List<Tessera> createTessera() {
+        List<Tessera> tesseraList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int numOfTessera;
+        while (true) {
+            System.out.println("Quante persone vuoi creare?");
+            try {
+                numOfTessera = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Inserisci un numero valido");
+
+            } catch (Exception e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+        try {
+            for (int i = 0; i < numOfTessera; i++) {
+                tesseraList.add(tesseraCreateOne.get());
+            }
+            System.out.println("Persone create con successo");
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+
+        return tesseraList;
     }
 
-    public static void createUtente(Scanner scanner, UtenteDAO utenteDAO, EntityManager em) {
-        createAndPersistEntities(utenteCreateOne, "persone", scanner, em, () -> utenteDAO.save(utenteCreateOne.get()));
+    public static List<Abbonamento> createAbbonamento() {
+        List<Abbonamento> abbonamentoList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int numOfAbbonamento;
+        while (true) {
+            System.out.println("quanti abbonamenti vuoi creare?");
+            try {
+                numOfAbbonamento = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("inserisci un numero valido");
+
+            } catch (Exception e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+        try {
+            for (int i = 0; i < numOfAbbonamento; i++) {
+                abbonamentoList.add(abbonamentoCreateOne.get());
+            }
+            System.out.println("Abbonamenti creati con successo");
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+
+        return abbonamentoList;
     }
 
-    public static void createTessera(Scanner scanner, TesseraDAO tesseraDAO, EntityManager em) {
-        createAndPersistEntities(tesseraCreateOne, "tessere", scanner, em, () -> tesseraDAO.save(tesseraCreateOne.get()));
-    }
+    public static List<Biglietto> createBiglietto() {
+        List<Biglietto> bigliettoList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int numOfBiglietto;
+        while (true) {
+            System.out.println("quanti biglietti vuoi creare?");
+            try {
+                numOfBiglietto = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("inserisci un numero valido");
 
-    public static void createAbbonamento(Scanner scanner, AbbonamentoDAO abbonamentoDAO, EntityManager em) {
-        createAndPersistEntities(abbonamentoCreateOne, "abbonamenti", scanner, em, () -> abbonamentoDAO.save(abbonamentoCreateOne.get()));
-    }
+            } catch (Exception e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+        try {
+            for (int i = 0; i < numOfBiglietto; i++) {
+                bigliettoList.add(bigliettoCreateOne.get());
+            }
+            System.out.println("Biglietti creati con successo");
+        } catch (Exception e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
 
-    public static void createBiglietto(Scanner scanner, BigliettoDAO bigliettoDAO, EntityManager em) {
-        createAndPersistEntities(bigliettoCreateOne, "biglietti", scanner, em, () -> bigliettoDAO.save(bigliettoCreateOne.get()));
+        return bigliettoList;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        EntityManager em = emf.createEntityManager();
-
-
-        UtenteDAO utenteDAO = new UtenteDAO(em);
-        TesseraDAO tesseraDAO = new TesseraDAO(em);
-        AbbonamentoDAO abbonamentoDAO = new AbbonamentoDAO(em);
-        BigliettoDAO bigliettoDAO = new BigliettoDAO(em);
-        PuntoDiEmissioneDAO puntoDiEmissioneDAO = new PuntoDiEmissioneDAO(em);
-
-
-        createDistributoriAutomatici(scanner, puntoDiEmissioneDAO, em);
-        createRivenditoriAutorizzati(scanner, puntoDiEmissioneDAO, em);
-        createUtente(scanner, utenteDAO, em);
-        createTessera(scanner, tesseraDAO, em);
-        createAbbonamento(scanner, abbonamentoDAO, em);
-        createBiglietto(scanner, bigliettoDAO, em);
-
-
-        em.close();
-        scanner.close();
+        System.out.println("Hello World!");
     }
+
 }
+
+
+
 
