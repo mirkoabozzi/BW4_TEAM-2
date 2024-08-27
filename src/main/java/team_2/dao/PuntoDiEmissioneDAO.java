e32bf620a02386d44af122da027676f999e645b6
Simone Pomponio <Pomponio671@gmail.com>
2024-08-27 10:51:46 +0200

    query TesseraDAO e PuntiDiEmissioneDAO

--- src/main/java/team_2/dao/PuntoDiEmissioneDAO.java
+++ src/main/java/team_2/dao/PuntoDiEmissioneDAO.java
@@ -2,10 +2,10 @@ package team_2.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
 import team_2.entities.PuntoDiEmissione;
 import team_2.exceptions.NotFoundException;
 
-import java.util.List;
 import java.util.UUID;
 
 public class PuntoDiEmissioneDAO {
@@ -23,16 +23,6 @@ public class PuntoDiEmissioneDAO {
         System.out.println("Punto di emissione con id " + puntoDiEmissione.getId() + " salvato nel DB");
     }
 
-    public void saveList(List<PuntoDiEmissione> puntoDiEmissioneList) {
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        for (PuntoDiEmissione puntoDiEmissione : puntoDiEmissioneList) {
-            em.persist(puntoDiEmissione);
-        }
-        transaction.commit();
-        System.out.println("Punti di emissione aggiunti nel DB");
-    }
-
     public PuntoDiEmissione getById(String id) {
         PuntoDiEmissione elementFound = em.find(PuntoDiEmissione.class, UUID.fromString(id));
         if (elementFound == null)
@@ -48,4 +38,32 @@ public class PuntoDiEmissioneDAO {
         transition.commit();
         System.out.println("Elemento con id " + elementFound.getId() + " eliminato");
     }
+    //Query
+
+    //1.Calcola quanti punti di Emissione ci sono:
+
+    public Long contaPuntiDiEmissione() {
+        String jpql = "SELECT COUNT(p) FROM PuntoDiEmissione p";
+        Query query = em.createQuery(jpql);
+        Long count = (Long) query.getSingleResult();
+        return count;
+    }
+    // 2.Calcola quanti RivenditoriAutorizzati ci sono:
+
+    public Long contaRivenditoriAutorizzati() {
+        String jpql = "SELECT COUNT(r) FROM RivenditoriAutorizzati r";
+        Query query = em.createQuery(jpql);
+        return (Long) query.getSingleResult();
+    }
+
+    // 2.1 Calcola quanti DistributoriAutomatici ci sono:
+
+    public Long contaDistributoriAutomatici() {
+        String jpql = "SELECT COUNT(d) FROM DistributoriAutomatici d";
+        Query query = em.createQuery(jpql);
+        return (Long) query.getSingleResult();
+
+
+    }
 }
+
