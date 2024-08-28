926c4293c1318b019e6d02db85755a57c255cee7
Simone Pomponio <Pomponio671@gmail.com>
2024-08-28 15:41:48 +0200

    query PuntiDiEmissione

--- src/main/java/team_2/dao/PuntoDiEmissioneDAO.java
+++ src/main/java/team_2/dao/PuntoDiEmissioneDAO.java
@@ -3,9 +3,13 @@ package team_2.dao;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import team_2.entities.PuntoDiEmissione;
+import team_2.entities.RivenditoriAutorizzati;
+import team_2.enums.StatoDistributori;
 import team_2.exceptions.NotFoundException;
 
+import java.util.List;
 import java.util.UUID;
 
 public class PuntoDiEmissioneDAO {
@@ -62,8 +66,27 @@ public class PuntoDiEmissioneDAO {
         String jpql = "SELECT COUNT(d) FROM DistributoriAutomatici d";
         Query query = em.createQuery(jpql);
         return (Long) query.getSingleResult();
-
-
     }
+
+    // 3. Cerca un RivenditoriAutorizzati tramite il nome
+
+    public List<RivenditoriAutorizzati> cercaRivenditoriAutorizzatiPerNome(String name) {
+        String jpql = "SELECT r FROM RivenditoriAutorizzati r WHERE r.name = :name";
+        TypedQuery<RivenditoriAutorizzati> query = em.createQuery(jpql, RivenditoriAutorizzati.class);
+        query.setParameter("name", name);
+        return query.getResultList();
+    }
+
+    // 4. Calcola quanti Distributori Automatici hanno stato Attivo
+
+    public Long DistributoriAutomaticiAttivi() {
+        String jpql = "SELECT COUNT(d) FROM DistributoriAutomatici d WHERE d.statoDistributori = :stato";
+        Query query = em.createQuery(jpql);
+        query.setParameter("stato", StatoDistributori.ATTIVO);
+        return (Long) query.getSingleResult();
+    }
+
+    
 }
+
 
